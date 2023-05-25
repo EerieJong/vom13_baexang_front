101d4eea380d2d7d9192ec0e9986038ab2645ef4
EerieJong <jongkwanja@gmail.com>
2023-05-25 12:27:40 +0900

    initial commit

---
+++ js/etc/include.js
@@ -0,0 +1,6 @@
+$(function () {
+  // const versionUpdate = new Date().getTime();
+  $('#header-wrapper').load(`/baexang_front/include/header.html`);
+  $('#mobile-overlay-wrapper').load(`/baexang_front/include/mobile-menus.html`);
+  $('#footer-wrapper').load(`/baexang_front/include/footer.html`);
+});
---
+++ js/etc/requestMethods.js
@@ -0,0 +1,39 @@
+/* ====== Common Post Request Function ====== */
+async function postRequest(url, options) {
+  return await fetch(url, options).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  });
+}
+
+/* ====== Common Put Request Function ====== */
+async function putRequest(url, options) {
+  return await fetch(url, options).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  });
+}
+
+/* ====== Common DELETE Request Function ====== */
+async function deleteRequest(url, options) {
+  return await fetch(url, options).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  });
+}
+
+/* ====== Common GET Request Function ====== */
+async function getRequest(url) {
+  return await fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  });
+}
